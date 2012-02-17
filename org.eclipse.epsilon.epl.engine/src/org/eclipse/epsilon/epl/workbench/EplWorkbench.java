64c8036ff7a7a0a3da77bddbc95fc40e57149b81
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2012-02-17 00:35:16 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@90 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- org.eclipse.epsilon.epl.engine/src/org/eclipse/epsilon/epl/workbench/EplWorkbench.java
+++ org.eclipse.epsilon.epl.engine/src/org/eclipse/epsilon/epl/workbench/EplWorkbench.java
@@ -12,7 +12,7 @@ import org.eclipse.epsilon.epl.PatternMatcher;
 public class EplWorkbench {
 	
 	public static void main(String[] args) throws Exception {
-		
+		/*
 		EplModule module = new EplModule();
 		module.parse(new File("/Users/dimitrioskolovos/Projects/Eclipse/3.7.1/workspace-new/org.eclipse.epsilon.epl.engine/src/org/eclipse/epsilon/epl/workbench/library.epl"));
 		
@@ -22,16 +22,16 @@ public class EplWorkbench {
 			}
 			System.exit(0);
 		}
-		
+		*/
 		PlainXmlModel model = new PlainXmlModel();
 		model.setName("L");
 		model.setFile(new File("/Users/dimitrioskolovos/Projects/Eclipse/3.7.1/workspace-new/org.eclipse.epsilon.epl.engine/src/org/eclipse/epsilon/epl/workbench/library.xml"));
 		model.load();
 		
-		module.getContext().getModelRepository().addModel(model);
-		module.getContext().setModule(module);
-		PatternMatchModel pmm = new PatternMatcher().match(module);
-		pmm.setName("EPL");
+		//module.getContext().getModelRepository().addModel(model);
+		//module.getContext().setModule(module);
+		//PatternMatchModel pmm = new PatternMatcher().match(module);
+		//pmm.setName("EPL");
 		
 		EolModule eolModule = new EolModule();
 		eolModule.parse(new File("/Users/dimitrioskolovos/Projects/Eclipse/3.7.1/workspace-new/org.eclipse.epsilon.epl.engine/src/org/eclipse/epsilon/epl/workbench/calllibraryepl.eol"));
@@ -44,7 +44,7 @@ public class EplWorkbench {
 		}
 		
 		eolModule.getContext().getModelRepository().addModel(model);
-		eolModule.getContext().getModelRepository().addModel(pmm);
+		//eolModule.getContext().getModelRepository().addModel(pmm);
 		eolModule.execute();
 		
 	}
