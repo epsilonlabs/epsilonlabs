88c723bee45416bc3bbdc59a54a0f02a75f210e6
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2012-11-15 21:15:37 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@258 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- org.eclipse.epsilon.live.webxeed/src/org/eclipse/epsilon/live/webxeed/client/EmfModelRetrievalServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.epsilon.live.webxeed.client;
-
-import org.eclipse.epsilon.live.webxeed.shared.WObject;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface EmfModelRetrievalServiceAsync {
-	public void getEmfModel(String modelUrl, String metamodelUrl, AsyncCallback<WObject> callback);
-}
