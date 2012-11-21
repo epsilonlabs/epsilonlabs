a325c85c318f19624b1a1711eaae9410f2228f1c
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2012-11-21 22:50:06 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@287 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- org.eclipse.epsilon.xminus/src/org/eclipse/epsilon/xminus/XMinusApp.java
+++ org.eclipse.epsilon.xminus/src/org/eclipse/epsilon/xminus/XMinusApp.java
@@ -1,5 +1,8 @@
 package org.eclipse.epsilon.xminus;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -14,25 +17,25 @@ import testm2.Testm2Package;
 public class XMinusApp {
 	
 	public static void main(String[] args) throws Exception {
-		
 		new XMinusApp().run();
-		
 	}
 	
 	public void run() throws Exception {
 		ResourceSet rs = new ResourceSetImpl();
 		rs.getPackageRegistry().put(Testm2Package.eINSTANCE.getNsURI(), Testm2Package.eINSTANCE);
 		rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XminusResourceFactory());
-		Resource r = rs.createResource(URI.createFileURI("/Users/dimitrioskolovos/Projects/Eclipse/appengine-juno/workspace/org.eclipse.epsilon.xminus/test.xml"));
+		Resource r = rs.createResource(URI.createFileURI(new File("test.xml").getAbsolutePath()));
 		r.load(null);
 		
 		EolModule module = new EolModule();
 		//module.parse("for (wp in WP.all) { wp.leader.id.println(wp.title + '->');}");
 		//module.parse("for (task in Task.all) { task.partners.size().println(); }");
-		//module.parse("Effort.all.collect(e|e.months).sum().println();");
+		module.parse("Effort.all.collect(e|e.months).sum().println();");
 		InMemoryEmfModel model = new InMemoryEmfModel(r);
 		module.getContext().getModelRepository().addModel(model);
 		module.execute();
+		
+		r.save(new FileOutputStream(new File("test.copy.xml")), null);
 		
 		/*
 		for (EObject o : r.getContents()) {
--- org.eclipse.epsilon.xminus/src/org/eclipse/epsilon/xminus/XminusResource.java
+++ org.eclipse.epsilon.xminus/src/org/eclipse/epsilon/xminus/XminusResource.java
@@ -10,6 +10,13 @@ import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
@@ -33,6 +40,7 @@ public class XminusResource extends ResourceImpl {
 	protected short ElementNodeType = 1;
 	protected List<EClass> eClasses = null;
 	protected HashMap<EObject, Node> eObjectNodes = new HashMap<EObject, Node>();
+	protected Document document = null;
 	
 	public XminusResource(URI uri) {
 		setURI(uri);
@@ -44,7 +52,7 @@ public class XminusResource extends ResourceImpl {
 		try {
 			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-			Document document = documentBuilder.parse(inputStream);
+			document = documentBuilder.parse(inputStream);
 			getContents().add(createEObject(document.getDocumentElement(), null));
 			resolveReferences();
 		}
@@ -57,7 +65,25 @@ public class XminusResource extends ResourceImpl {
 	@Override
 	protected void doSave(OutputStream outputStream, Map<?, ?> options)
 			throws IOException {
-		super.doSave(outputStream, options);
+		
+		try {
+			Source xmlSource = new DOMSource(document);
+			Result result = new StreamResult(outputStream);
+	
+			// create TransformerFactory
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			
+			// create Transformer for transformation
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");	//Java XML Indent
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			
+			// transform and deliver content to client
+			transformer.transform(xmlSource, result);
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 	
 	protected void resolveReferences() {
