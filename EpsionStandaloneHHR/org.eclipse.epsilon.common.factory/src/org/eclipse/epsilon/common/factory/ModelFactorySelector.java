de9c8961a12e9d13499758e75e3ae024ae0181c9
Horacio Hoyos-Rodriguez <hhr502@york.ac.uk>
2012-12-14 12:10:06 +0000

    - Changed the getModelFactory method to use the "driver" name to dynamically create the specific factory class. The EpsilonConfigurations should now save their associated Factory in the parameters map. The getDriver in the configuration should be changed to getFactoryName or such.
    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@292 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- EpsionStandaloneHHR/org.eclipse.epsilon.common.factory/src/org/eclipse/epsilon/common/factory/ModelFactorySelector.java
+++ EpsionStandaloneHHR/org.eclipse.epsilon.common.factory/src/org/eclipse/epsilon/common/factory/ModelFactorySelector.java
@@ -6,45 +6,54 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     hhoyos - initial API and implementation
+ *     Horacio Hoyos - initial API and implementation
  ******************************************************************************/
 package org.eclipse.epsilon.common.factory;
-
-import org.eclipse.epsilon.emc.emf.factory.EpsilonEmfModelFactory;
 
 
 public final class ModelFactorySelector {
 	
+	public static final String BIBTEX_DRIVER = "bibtex";
+	public static final String CSV_DRIVER = "csv";
+	public static final String EMF_DRIVER = "emf";
+	public static final String HUTN_DRIVER = "hutn";
+	public static final String PLAINXML_DRIVER = "plainxml";
 	
-	public static EpsilonModelFactory getModelFactory( EpsilonModelConfig epsilonModelConfig ){
+	public static Class<?> getModelFactory( EpsilonModelConfig epsilonModelConfig ) throws ClassNotFoundException {
 		
 		if ( epsilonModelConfig == null ) {
-	      throw new IllegalArgumentException("EpsilonModelConfig must not be null.");
+			throw new IllegalArgumentException("EpsilonModelConfig must not be null.");
 	    }
-	    //examine the configuration to extract the emc driver identifier
-	    final String driver = epsilonModelConfig.getDriverName();
-	    if ( driver.equalsIgnoreCase("bitex")) {
+	    //examine the configuration to extract the emc driver identifier.
+	    final String factoryName = epsilonModelConfig.getDriverName();
+	    if (factoryName == null) {
+	    	throw new NullPointerException("The model configuration does not specified a assocaited factory");
+	    }
+	    // TODO Can the driver identifier be instead the factory class name?
+	    // That way we just create a new class of the correct type.
+	 	return Class.forName(factoryName);
+	 	/*if ( driver.equalsIgnoreCase(BIBTEX_DRIVER)) {
 	    	// TODO implement the bibtex factory
 	    	throw new UnsupportedOperationException("The bibtex factory is not yet implemented");
 	    }
-	    else if ( driver.equalsIgnoreCase("csv") ) {
+	    else if ( driver.equalsIgnoreCase(CSV_DRIVER) ) {
 	    	// TODO implement the bibtex factory
 	    	throw new UnsupportedOperationException("The csv factory is not yet implemented");
 	    }
-	    else if ( driver.equalsIgnoreCase("emf") ) {
+	    else if ( driver.equalsIgnoreCase(EMF_DRIVER) ) {
 	    	return new EpsilonEmfModelFactory();
 	    }
-	    else if ( driver.equalsIgnoreCase("hutn") ) {
+	    else if ( driver.equalsIgnoreCase(HUTN_DRIVER) ) {
 	    	// TODO implement the hutn factory
 	    	throw new UnsupportedOperationException("The hutn factory is not yet implemented");
 	    }
-	    else if ( driver.equalsIgnoreCase("plainxml") ) {
+	    else if ( driver.equalsIgnoreCase(PLAINXML_DRIVER) ) {
 	    	// TODO implement the plainxml factory
 	    	throw new UnsupportedOperationException("The plainxml factory is not yet implemented");
 	    }
 	    else {
 	      throw new IllegalArgumentException("Unknown emc driver identifier.");
-	    }
+	    }*/
 	}
 
 }
