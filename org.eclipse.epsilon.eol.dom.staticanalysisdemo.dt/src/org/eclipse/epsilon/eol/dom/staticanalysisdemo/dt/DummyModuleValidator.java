ee38a8ad8001e66f2268aa840992c694456824fa
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2013-07-09 20:28:12 +0000

    Updated module validator example to detect for loops and if statements
    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@409 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- org.eclipse.epsilon.eol.dom.staticanalysisdemo.dt/src/org/eclipse/epsilon/eol/dom/staticanalysisdemo/dt/DummyModuleValidator.java
+++ org.eclipse.epsilon.eol.dom.staticanalysisdemo.dt/src/org/eclipse/epsilon/eol/dom/staticanalysisdemo/dt/DummyModuleValidator.java
@@ -1,12 +1,14 @@
 package org.eclipse.epsilon.eol.dom.staticanalysisdemo.dt;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.epsilon.common.module.IModule;
 import org.eclipse.epsilon.common.module.IModuleValidator;
 import org.eclipse.epsilon.common.module.ModuleMarker;
 import org.eclipse.epsilon.common.module.ModuleMarker.Severity;
+import org.eclipse.epsilon.common.parse.AST;
+import org.eclipse.epsilon.eol.parse.EolParser;
 
 public class DummyModuleValidator implements IModuleValidator {
 
@@ -15,7 +17,20 @@ public class DummyModuleValidator implements IModuleValidator {
 
 	@Override
 	public List<ModuleMarker> validate(IModule module) {
-		return Arrays.asList(new ModuleMarker(null, module.getAst().getFirstChild().getRegion(), "Dummy warning message", Severity.Warning));
+		ArrayList<ModuleMarker> markers = new ArrayList<ModuleMarker>();
+		
+		// Creates dummy warning markers for all "for" statements
+		// and error markers for all "if" statements
+		for (AST ast : module.getAst().getDescendants()) {
+			if (ast.getType() == EolParser.FOR) {
+				// file can be null - doesn't matter yet
+				markers.add(new ModuleMarker(null, ast.getRegion(), "For loops are discouraged", Severity.Warning));
+			}
+			else if (ast.getType() == EolParser.IF) {
+				markers.add(new ModuleMarker(null, ast.getRegion(), "If statements are out of question", Severity.Error));				
+			}
+		}
+		return markers;
 	}
 
 }
