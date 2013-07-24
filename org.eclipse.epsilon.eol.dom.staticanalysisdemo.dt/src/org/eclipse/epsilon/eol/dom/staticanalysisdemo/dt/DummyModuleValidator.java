18b9e60c0cceed73e74d9da4b3f22b9a118feb41
Ran Wei <ran.wei@york.ac.uk>
2013-07-24 14:04:03 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@426 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- org.eclipse.epsilon.eol.dom.staticanalysisdemo.dt/src/org/eclipse/epsilon/eol/dom/staticanalysisdemo/dt/DummyModuleValidator.java
+++ org.eclipse.epsilon.eol.dom.staticanalysisdemo.dt/src/org/eclipse/epsilon/eol/dom/staticanalysisdemo/dt/DummyModuleValidator.java
@@ -8,6 +8,13 @@ import org.eclipse.epsilon.common.module.IModuleValidator;
 import org.eclipse.epsilon.common.module.ModuleMarker;
 import org.eclipse.epsilon.common.module.ModuleMarker.Severity;
 import org.eclipse.epsilon.common.parse.AST;
+import org.eclipse.epsilon.common.parse.Region;
+import org.eclipse.epsilon.eol.dom.DomElement;
+import org.eclipse.epsilon.eol.dom.TextRegion;
+import org.eclipse.epsilon.eol.dom.ast2dom.Ast2DomContext;
+import org.eclipse.epsilon.eol.dom.ast2dom.EolElementCreatorFactory;
+import org.eclipse.epsilon.eol.dom.visitor.resolution.type.impl.TypeResolver;
+import org.eclipse.epsilon.eol.dom.visitor.resolution.variable.impl.VariableResolver;
 import org.eclipse.epsilon.eol.parse.EolParser;
 
 public class DummyModuleValidator implements IModuleValidator {
@@ -21,7 +28,7 @@ public class DummyModuleValidator implements IModuleValidator {
 		
 		// Creates dummy warning markers for all "for" statements
 		// and error markers for all "if" statements
-		for (AST ast : module.getAst().getDescendants()) {
+		/*for (AST ast : module.getAst().getDescendants()) {
 			if (ast.getType() == EolParser.FOR) {
 				// file can be null - doesn't matter yet
 				markers.add(new ModuleMarker(null, ast.getRegion(), "For loops are discouraged", Severity.Warning));
@@ -29,8 +36,38 @@ public class DummyModuleValidator implements IModuleValidator {
 			else if (ast.getType() == EolParser.IF) {
 				markers.add(new ModuleMarker(null, ast.getRegion(), "If statements are out of question", Severity.Error));				
 			}
+		}*/
+		
+		DomElement dom = new EolElementCreatorFactory().createDomElement(module.getAst(), null, new Ast2DomContext());
+		VariableResolver vr = new VariableResolver();
+		vr.run(dom);
+		
+		TypeResolver tr = new TypeResolver();
+		tr.run(dom);
+		
+		for(log.Error error: tr.getTypeResolutionContext().getLogBook().getErrors())
+		{
+			TextRegion textRegion = error.getDomElement().getRegion();
+			Region region = new Region(textRegion.getStart().getLine(), textRegion.getStart().getColumn(), textRegion.getEnd().getLine(), textRegion.getEnd().getColumn());
+			ModuleMarker marker = new ModuleMarker(null, region, error.getMessage(), Severity.Error);
+			markers.add(marker);
 		}
+		for(log.Warning warning: tr.getTypeResolutionContext().getLogBook().getWarnings())
+		{
+			TextRegion textRegion = warning.getDomElement().getRegion();
+			Region region = new Region(textRegion.getStart().getLine(), textRegion.getStart().getColumn(), textRegion.getEnd().getLine(), textRegion.getEnd().getColumn());
+
+			ModuleMarker marker = new ModuleMarker(null, region, warning.getMessage(), Severity.Warning);
+			markers.add(marker);
+		}
+		
 		return markers;
+	}
+
+	@Override
+	public String getMarkerType() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
