5d137e45b35c600958c0e84349190a9fbbb95640
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2013-10-26 10:50:47 +0000

    Added support for profiles to the ArgoUML driver
    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@489 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- org.eclipse.epsilon.emc.argouml.dt/src/org/eclipse/epsilon/emc/argouml/dt/ArgoUMLModelConfigurationDialog.java
+++ org.eclipse.epsilon.emc.argouml.dt/src/org/eclipse/epsilon/emc/argouml/dt/ArgoUMLModelConfigurationDialog.java
@@ -35,56 +35,45 @@ public class ArgoUMLModelConfigurationDialog extends AbstractCachedModelConfigur
 	
 	protected Label fileTextLabel;
 	protected Text fileText;
-	//protected Label uriTextLabel;
-	//protected Text uriText;
+	protected Label profileDirectoriesLabel;
+	protected Text profileDirectoriesText;
 	protected Button browseModelFile;
-	//protected Button filebasedButton;
+	protected Button overrideProfilesButton;
 	
 	protected void createGroups(Composite control) {
 		super.createGroups(control);
 		createFilesGroup(control);
+		createProfilesGroup(control);
 		createLoadStoreOptionsGroup(control);
-		//toggleEnabledFields();
 	}
 	
-	/*
-	protected void toggleEnabledFields() {
-		if (filebasedButton.getSelection()) {
-			fileTextLabel.setEnabled(true);
-			fileText.setEnabled(true);
-			uriTextLabel.setEnabled(false);
-			uriText.setEnabled(false);
-			uriText.setText("");
-		}
-		else {
-			fileTextLabel.setEnabled(false);
-			fileText.setEnabled(false);
-			uriTextLabel.setEnabled(true);
-			uriText.setEnabled(true);
-			fileText.setText("");
-			storeOnDisposalCheckbox.setSelection(false);
-		}
-	}*/
+	protected Composite createProfilesGroup(Composite parent) {
+		final Composite groupContent = createGroupContainer(parent, "Profiles", 3);
+		
+		overrideProfilesButton = new Button(groupContent, SWT.CHECK);
+		GridData filebasedButtonGridData = new GridData(GridData.FILL_HORIZONTAL);
+		filebasedButtonGridData.horizontalSpan = 3;
+		overrideProfilesButton.setSelection(false);
+		overrideProfilesButton.setText("Override ArgoUML's profile directories");
+		overrideProfilesButton.setToolTipText("Overrides ArgoUML's configured profile directories");
+		overrideProfilesButton.setLayoutData(filebasedButtonGridData);
+		
+		profileDirectoriesLabel = new Label(groupContent, SWT.NONE);
+		profileDirectoriesLabel.setText("Profile directories: ");
+		profileDirectoriesLabel.setToolTipText("Directories in which the driver should search for available profiles. Split multiple directories with ;");
+		
+		profileDirectoriesText = new Text(groupContent, SWT.BORDER);
+		GridData uriTextGridData = new GridData(GridData.FILL_HORIZONTAL);
+		uriTextGridData.horizontalSpan = 2;
+		profileDirectoriesText.setLayoutData(uriTextGridData);
+		
+		groupContent.layout();
+		groupContent.pack();
+		return groupContent;
+	}
 	
 	protected Composite createFilesGroup(Composite parent) {
 		final Composite groupContent = createGroupContainer(parent, "Files/URIs", 3);
-		
-		/*
-		filebasedButton = new Button(groupContent, SWT.CHECK);
-		GridData filebasedButtonGridData = new GridData(GridData.FILL_HORIZONTAL);
-		filebasedButtonGridData.horizontalSpan = 3;
-		filebasedButton.setSelection(true);
-		filebasedButton.setText("Workspace file");
-		filebasedButton.setLayoutData(filebasedButtonGridData);
-		filebasedButton.addListener(SWT.Selection, new Listener() {
-
-			
-			public void handleEvent(Event event) {
-				toggleEnabledFields();
-			}
-			
-		});*/
-		
 		
 		fileTextLabel = new Label(groupContent, SWT.NONE);
 		fileTextLabel.setText("File: ");
@@ -95,16 +84,6 @@ public class ArgoUMLModelConfigurationDialog extends AbstractCachedModelConfigur
 		browseModelFile = new Button(groupContent, SWT.NONE);
 		browseModelFile.setText("Browse Workspace...");
 		browseModelFile.addListener(SWT.Selection, new BrowseWorkspaceForModelsListener(fileText, "XML Documents in the workspace", "Select an XML document"));
-
-		/*
-		uriTextLabel = new Label(groupContent, SWT.NONE);
-		uriTextLabel.setText("URI: ");
-		
-		uriText = new Text(groupContent, SWT.BORDER);
-		GridData uriTextGridData = new GridData(GridData.FILL_HORIZONTAL);
-		uriTextGridData.horizontalSpan = 2;
-		uriText.setLayoutData(uriTextGridData);
-		*/
 		
 		groupContent.layout();
 		groupContent.pack();
@@ -117,16 +96,16 @@ public class ArgoUMLModelConfigurationDialog extends AbstractCachedModelConfigur
 		super.loadProperties();
 		if (properties == null) return;
 		fileText.setText(properties.getProperty(ArgoUMLModel.PROPERTY_MODEL_FILE));
-		//uriText.setText(properties.getProperty(PlainXmlModel.PROPERTY_URI));
-		//filebasedButton.setSelection(properties.getBooleanProperty("fileBased", true));
+		profileDirectoriesText.setText(properties.getProperty(ArgoUMLModel.PROPERTY_PROFILE_DIRECTORIES));
+		overrideProfilesButton.setSelection(properties.getBooleanProperty(ArgoUMLModel.PROPERTY_OVERRIDE_PROFILE_DIRECTORIES, false));
 		//toggleEnabledFields();
 	}
 	
 	
 	protected void storeProperties(){
 		super.storeProperties();
-		//properties.put(PlainXmlModel.PROPERTY_URI, uriText.getText());
+		properties.put(ArgoUMLModel.PROPERTY_PROFILE_DIRECTORIES, profileDirectoriesText.getText());
 		properties.put(ArgoUMLModel.PROPERTY_MODEL_FILE, fileText.getText());
-		//properties.put("fileBased", filebasedButton.getSelection() + "");
+		properties.put(ArgoUMLModel.PROPERTY_OVERRIDE_PROFILE_DIRECTORIES, overrideProfilesButton.getSelection() + "");
 	}
 }
--- org.eclipse.epsilon.emc.argouml/src/org/eclipse/epsilon/emc/argouml/ArgoUMLModel.java
+++ org.eclipse.epsilon.emc.argouml/src/org/eclipse/epsilon/emc/argouml/ArgoUMLModel.java
@@ -10,9 +10,11 @@ import javax.xml.transform.TransformerFactory;
 import org.argouml.application.Main;
 import org.argouml.configuration.Configuration;
 import org.argouml.kernel.Project;
+import org.argouml.kernel.ProjectManager;
 import org.argouml.notation.providers.uml.InitNotationUml;
 import org.argouml.persistence.AbstractFilePersister;
 import org.argouml.persistence.PersistenceManager;
+import org.argouml.profile.Profile;
 import org.argouml.profile.ProfileFacade;
 import org.argouml.profile.ProfileManager;
 import org.argouml.profile.internal.ProfileManagerImpl;
@@ -20,7 +22,11 @@ import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.epsilon.eol.EolModule;
 import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
 import org.eclipse.epsilon.eol.models.CachedModel;
+import org.omg.uml.foundation.core.ModelElement;
+import org.omg.uml.foundation.core.Stereotype;
+import org.omg.uml.foundation.core.UmlClass;
 import org.omg.uml.modelmanagement.Model;
+import org.omg.uml.modelmanagement.UmlPackage;
 
 public class ArgoUMLModel extends AbstractMdrModel {
 
@@ -34,6 +40,8 @@ public class ArgoUMLModel extends AbstractMdrModel {
 	protected AbstractFilePersister persister = null;
 	protected boolean overrideProfileDirectories;
 	protected List<String> profileDirectories = new ArrayList<String>();
+	protected ProfileManager profileManager = null;
+	protected Model model = null;
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -61,7 +69,73 @@ public class ArgoUMLModel extends AbstractMdrModel {
 	public RefPackage getRefPackage() {
 		return refPackage;
 	}
+	
+	public Profile applyProfile(String profileName) {
+		Profile profile = null;
+		
+		// Check to see whether the project already has a profile with this name
+		for (Profile p : project.getProfileConfiguration().getProfiles()) {
+			if (p.getProfileIdentifier().equals(profileName)) {
+				return p;
+			}
+		}
+		
+		// Find the profile
+		for (Profile p : profileManager.getRegisteredProfiles()) {
+			if (p.getProfileIdentifier().equals(profileName)) {
+				profile = p;
+				break;
+			}
+		}
+		
+		if (profile == null) throw new RuntimeException("Could not find profile " + profileName);
+		
+		project.getProfileConfiguration().addProfile(profile, model);
+		return profile;
+	}
+	
+	public Stereotype getStereotype(String stereotypeName) {
+		for (Profile p : project.getProfileConfiguration().getProfiles()) {
+			Stereotype stereotype = getStereotype(p.getProfileIdentifier(), stereotypeName);
+			if (stereotype != null) {
+				return stereotype;
+			}
+		}
+		return null;
+	}
+	
+	public Stereotype getStereotype(String profileName, String stereotypeName) {
+		
+		Profile profile = null;
+		
+		for (Profile p : project.getProfileConfiguration().getProfiles()) {
+			if (p.getProfileIdentifier().equals(profileName)) {
+				profile = p;
+				break;
+			}
+		}
+		
+		if (profile == null) return null;
 
+		try {
+			for (Object o : profile.getProfilePackages()) {
+				UmlPackage profilePackage = (UmlPackage) o;
+				for (Object ownedElememnt : profilePackage.getOwnedElement()) {
+					if (ownedElememnt instanceof Stereotype) {
+						Stereotype stereotype = (Stereotype) ownedElememnt;
+						if (stereotype.getName().equals(stereotypeName)) {
+							return stereotype;
+						}
+					}
+				}
+			}
+		}
+		catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		return null;
+	}
+	
 	@Override
 	public void load(StringProperties properties, String basePath)
 			throws EolModelLoadingException {
@@ -76,8 +150,10 @@ public class ArgoUMLModel extends AbstractMdrModel {
 		cacheElements = properties.getBooleanProperty(CachedModel.PROPERTY_CACHED, true);
 		overrideProfileDirectories = properties.getBooleanProperty(PROPERTY_OVERRIDE_PROFILE_DIRECTORIES, false);
 		String profileDirectoriesString = properties.getProperty(PROPERTY_PROFILE_DIRECTORIES, "");
-		for (String profileDirectory : profileDirectoriesString.split(";")) {
-			profileDirectories.add(profileDirectory.trim());
+		if (profileDirectoriesString.trim().length() > 0) {
+			for (String profileDirectory : profileDirectoriesString.split(";")) {
+				profileDirectories.add(profileDirectory.trim());
+			}
 		}
 		
 		load();
@@ -87,9 +163,6 @@ public class ArgoUMLModel extends AbstractMdrModel {
 	public void load() throws EolModelLoadingException {
 		
 		try {
-			
-			//System.err.println(" --- " + TransformerFactory.newInstance().getClass().getCanonicalName());
-			
 			File file = new File(modelFile);
 
 			Configuration.load();
@@ -98,7 +171,7 @@ public class ArgoUMLModel extends AbstractMdrModel {
 			persister = pm.getPersisterFromFileName(file.getAbsolutePath());
 			InitializeModel.initializeDefault();
 			
-			ProfileManager profileManager = new ProfileManagerImpl() {
+			profileManager = new ProfileManagerImpl() {
 				
 				public void refreshRegisteredProfiles() {
 					
@@ -116,11 +189,22 @@ public class ArgoUMLModel extends AbstractMdrModel {
 				};
 			};
 			
+			
 			ProfileFacade.setManager(profileManager);
 			new InitNotationUml().init();
-			project = persister.doLoad(file);
-
-			Model model = (Model) project.getUserDefinedModelList().get(0);	
+			
+			for (Project oldProject : new ArrayList<Project>(ProjectManager.getManager().getOpenProjects())) {
+				ProjectManager.getManager().removeProject(oldProject);
+			}
+			
+			if (readOnLoad) {
+				project = persister.doLoad(file);
+			}
+			else {
+				project = ProjectManager.getManager().makeEmptyProject(true);
+			}
+			
+			model = (Model) project.getUserDefinedModelList().get(0);	
 			refPackage = model.refClass().refOutermostPackage();
 			
 		}
@@ -128,7 +212,6 @@ public class ArgoUMLModel extends AbstractMdrModel {
 			throw new EolModelLoadingException(ex, this);
 		}
 		finally {
-			//System.setProperty("javax.xml.transform.TransformerFactory", "null");
 			System.clearProperty("javax.xml.transform.TransformerFactory");
 		}
 			
