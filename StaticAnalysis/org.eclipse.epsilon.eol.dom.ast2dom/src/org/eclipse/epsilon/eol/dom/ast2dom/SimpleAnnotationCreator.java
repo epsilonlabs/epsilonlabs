1e936e542c9db1366563275e22662ace59185718
Ran Wei <ran.wei@york.ac.uk>
2013-12-16 12:51:54 +0000

    bug fix for @cached annotation
    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@523 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- StaticAnalysis/org.eclipse.epsilon.eol.dom.ast2dom/src/org/eclipse/epsilon/eol/dom/ast2dom/SimpleAnnotationCreator.java
+++ StaticAnalysis/org.eclipse.epsilon.eol.dom.ast2dom/src/org/eclipse/epsilon/eol/dom/ast2dom/SimpleAnnotationCreator.java
@@ -21,17 +21,20 @@ public class SimpleAnnotationCreator extends AnnotationCreator{
 		if (arr.length != 0) {
 			String nameString = arr[0]; //the name string should be the first one
 			nameString = nameString.substring(1);  //truncate "@"
-			String valueString = astText.substring(nameString.length()+2); //the value string should be the rest
+			if (arr.length > 1) {
+				String valueString = astText.substring(nameString.length()+2); //the value string should be the rest
+				StringExpression value = (StringExpression) context.getEolElementCreatorFactory().createDomElement(ast, annotation, context, StringExpressionCreator.class);
+				value.setColumn(ast.getColumn() + nameString.length()); //set column
+				value.setVal(valueString); //set value of the value
+				annotation.getValues().add(value); //set the value of the annotation
+			}
 			
 			NameExpression name = (NameExpression) context.getEolElementCreatorFactory().createDomElement(ast, annotation, context, NameExpressionCreator.class);
 			name.setName(nameString); //set name to the NameExpression
 			
 			annotation.setName(name); //set name for the annotation
 			
-			StringExpression value = (StringExpression) context.getEolElementCreatorFactory().createDomElement(ast, annotation, context, StringExpressionCreator.class);
-			value.setColumn(ast.getColumn() + nameString.length()); //set column
-			value.setVal(valueString); //set value of the value
-			annotation.getValues().add(value); //set the value of the annotation
+			
 		}
 		return annotation;
 	}
