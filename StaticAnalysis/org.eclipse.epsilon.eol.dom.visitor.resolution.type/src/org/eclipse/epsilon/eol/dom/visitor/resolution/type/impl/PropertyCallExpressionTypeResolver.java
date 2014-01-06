6bf6c6e761f248835e57269a4689acb8e299fbc7
Ran Wei <ran.wei@york.ac.uk>
2014-01-06 15:32:49 +0000

    fix for issue 13
    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@540 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- StaticAnalysis/org.eclipse.epsilon.eol.dom.visitor.resolution.type/src/org/eclipse/epsilon/eol/dom/visitor/resolution/type/impl/PropertyCallExpressionTypeResolver.java
+++ StaticAnalysis/org.eclipse.epsilon.eol.dom.visitor.resolution.type/src/org/eclipse/epsilon/eol/dom/visitor/resolution/type/impl/PropertyCallExpressionTypeResolver.java
@@ -162,6 +162,130 @@ public class PropertyCallExpressionTypeResolver extends PropertyCallExpressionVi
 				}
 			}
 			else {
+				if (propertyCallExpression.getTarget().getResolvedType() instanceof CollectionType) { //if target is of type CollectionType
+					CollectionType rawCollectionType = (CollectionType) propertyCallExpression.getTarget().getResolvedType(); //prepare collection type
+					
+					CollectionType rawResultType = EcoreUtil.copy(rawCollectionType);
+					
+					if (rawCollectionType.getContentType() instanceof ModelElementType) 
+					{ //if contentType is ModelElementType
+						ModelElementType resultContentType = (ModelElementType) rawCollectionType.getContentType(); //prepare result content type
+						EMetaModel mm = context.getMetaModel(resultContentType.getModelName()); //get the metamodel
+						if(mm != null) //if meta model exists
+						{
+							String metaClassString = resultContentType.getElementName(); //get metaclass string
+							String propertyString = propertyCallExpression.getProperty().getName(); //get property string
+							
+							if(mm.contains(metaClassString, propertyString)) //if metamode class contains the property
+							{
+								EStructuralFeature feature = mm.getEStructuralFeature(mm.getMetaClass(metaClassString), propertyString); //get the property 
+								propertyCallExpression.getProperty().setResolvedContent(feature); //set the resolved content for the property
+
+								if (feature.getUpperBound() != 1) { //this means that the feature is a many value aggregation
+									Type contentType = null; //each collection type needs a content type
+									CollectionType callType = null; //prepare the callType
+									
+									EClassifier propertyType = mm.getTypeForProperty(mm.getMetaClass(metaClassString), propertyString); //get the type for the property
+									if (context.getTypeUtil().isEDataType(propertyType)) { //if type is EDataType
+										if(context.getTypeUtil().isNormalisable(propertyType)){ //if type is normalisable
+											contentType = context.getTypeUtil().normalise(propertyType); //normalise and assign type to contentType
+										}
+										else { //if type is not normalisable
+											contentType = context.getEolFactory().createEType(); //create a EType and assign it to contentType
+											((EType) contentType).setEcoreType(propertyType); //assign the EDataType to EType
+										}
+									}
+									else { //if type is not EDatatype
+										contentType = context.getEolFactory().createModelElementType(); //assign a ModelElementType to contentType 
+										((ModelElementType) contentType).setEcoreType(propertyType); //setEcoreType
+										((ModelElementType) contentType).setModelName(mm.getMetaModelName()); //model name
+										((ModelElementType) contentType).setElementName(propertyType.getName()); //element name
+									}
+
+						
+									
+									if (feature.isOrdered() && feature.isUnique()) { //if feature is ordered and unique
+										callType = context.getEolFactory().createOrderedSetType(); //this should be ordered set
+									}
+									else if (feature.isOrdered() && !feature.isUnique()) { //if feature is ordered but not unique
+										callType = context.getEolFactory().createSequenceType(); //this should be a sequence
+									}
+									else if (!feature.isOrdered() && feature.isUnique()) {//if feature is unordered and unique
+										callType = context.getEolFactory().createSetType();  //this should be a set
+									}
+									else if (!feature.isOrdered() && !feature.isUnique()) {//if feature is unordered and non-unique
+										callType = context.getEolFactory().createBagType(); //this should be a bag
+									}
+									
+									callType.setContentType(contentType);
+									context.setAssets(contentType, callType); //set assets for contentType
+									
+									Type typeCopy = EcoreUtil.copy(callType);
+									propertyCallExpression.getProperty().setResolvedType(typeCopy);
+									context.setAssets(typeCopy, propertyCallExpression.getProperty());
+									
+									rawResultType.setContentType(callType);
+									context.setAssets(callType, rawResultType);
+									
+									propertyCallExpression.setResolvedType(rawResultType);
+									context.setAssets(rawResultType, propertyCallExpression); //set assets for callType
+									
+								}
+								else { //if the feature is single value aggregation
+									
+									EClassifier propertyType = mm.getTypeForProperty(mm.getMetaClass(metaClassString), propertyString); //get property
+									if (context.getTypeUtil().isEDataType(propertyType)) { //if property is data type
+										if(context.getTypeUtil().isNormalisable(propertyType)){ //if the data type is normalisable
+											Type type = context.getTypeUtil().normalise(propertyType);
+											propertyCallExpression.getProperty().setResolvedType(type);
+											context.setAssets(type, propertyCallExpression.getProperty());
+											
+											Type typeCopy = EcoreUtil.copy(type);
+											rawResultType.setContentType(typeCopy);
+											context.setAssets(typeCopy, rawResultType);
+											
+											propertyCallExpression.setResolvedType(rawResultType);
+											context.setAssets(rawResultType, propertyCallExpression);
+										}
+										else { //if the data type is not normalisable
+											EType eType = context.getEolFactory().createEType();
+											eType.setEcoreType(propertyType);
+											propertyCallExpression.getProperty().setResolvedType(eType);
+											context.setAssets(eType, propertyCallExpression.getProperty());
+											
+											Type typeCopy = EcoreUtil.copy(eType);
+											rawResultType.setContentType(typeCopy);
+											context.setAssets(typeCopy, rawResultType);
+											
+											propertyCallExpression.setResolvedType(rawResultType);
+											context.setAssets(rawResultType, propertyCallExpression);
+										}
+									}
+									else { //if the property is not data type, then it should be model element type
+										ModelElementType callType = context.getEolFactory().createModelElementType();
+										callType.setEcoreType(propertyType);
+										callType.setModelName(mm.getMetaModelName());
+										callType.setElementName(propertyType.getName());
+										propertyCallExpression.getProperty().setResolvedType(callType);
+										context.setAssets(callType, propertyCallExpression.getProperty());
+										
+										Type typeCopy = EcoreUtil.copy(callType);
+										rawResultType.setContentType(typeCopy);
+										context.setAssets(typeCopy, rawResultType);
+										
+										propertyCallExpression.setResolvedType(rawResultType);
+										context.setAssets(rawResultType, propertyCallExpression);
+									}
+								}
+							}
+							else {
+								context.getLogBook().addError(propertyCallExpression.getProperty(), "Property with name " + propertyString
+										+ " is not found" );
+
+							}
+						}
+					}
+				}
 				//no further property queries can be performed if the target type is not ModelElementType
 			}
 		}
