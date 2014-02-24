efffff26b5f0a35b6f973fde699ec5857bce86a2
Ran Wei <ran.wei@york.ac.uk>
2014-02-24 10:28:24 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@565 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

---
+++ StaticAnalysis/org.eclipse.epsilon.eol.dom/src/org/eclipse/epsilon/eol/dom/OrderedCollectionType.java
@@ -0,0 +1,17 @@
+/**
+ */
+package org.eclipse.epsilon.eol.dom;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Ordered Collection Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.eclipse.epsilon.eol.dom.EolPackage#getOrderedCollectionType()
+ * @model
+ * @generated
+ */
+public interface OrderedCollectionType extends CollectionType {
+} // OrderedCollectionType
---
+++ StaticAnalysis/org.eclipse.epsilon.eol.dom/src/org/eclipse/epsilon/eol/dom/SequenceType.java
@@ -0,0 +1,17 @@
+/**
+ */
+package org.eclipse.epsilon.eol.dom;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Sequence Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.eclipse.epsilon.eol.dom.EolPackage#getSequenceType()
+ * @model
+ * @generated
+ */
+public interface SequenceType extends OrderedCollectionType {
+} // SequenceType
