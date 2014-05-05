a419f8b62aa762994f8dd6d334eff56be1117945
Ran Wei <ran.wei@york.ac.uk>
2014-05-05 14:12:53 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@656 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

---
+++ StaticAnalysis/org.eclipse.epsilon.eol.metamodel/src/org/eclipse/epsilon/eol/metamodel/CollectionInitValue.java
@@ -0,0 +1,17 @@
+/**
+ */
+package org.eclipse.epsilon.eol.metamodel;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Collection Init Value</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.eclipse.epsilon.eol.metamodel.EolPackage#getCollectionInitValue()
+ * @model abstract="true"
+ * @generated
+ */
+public interface CollectionInitValue extends EolElement {
+} // CollectionInitValue
