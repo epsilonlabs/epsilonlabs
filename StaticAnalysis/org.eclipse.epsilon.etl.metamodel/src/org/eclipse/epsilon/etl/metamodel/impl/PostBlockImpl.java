1510bfc4e9ef293c80de7b593f4f11f07c95014f
Ran Wei <ran.wei@york.ac.uk>
2014-05-26 14:39:18 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@713 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

---
+++ StaticAnalysis/org.eclipse.epsilon.etl.metamodel/src/org/eclipse/epsilon/etl/metamodel/impl/PostBlockImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package org.eclipse.epsilon.etl.metamodel.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.epsilon.etl.metamodel.EtlPackage;
+import org.eclipse.epsilon.etl.metamodel.PostBlock;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Post Block</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class PostBlockImpl extends NamedBlockImpl implements PostBlock {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected PostBlockImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return EtlPackage.Literals.POST_BLOCK;
+	}
+
+} //PostBlockImpl
