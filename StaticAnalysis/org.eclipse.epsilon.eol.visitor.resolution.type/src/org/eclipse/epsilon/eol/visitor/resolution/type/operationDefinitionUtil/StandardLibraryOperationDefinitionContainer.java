403dcea8db2cf1de0e70d085f55f3cf0062c7b41
Ran Wei <ran.wei@york.ac.uk>
2014-06-04 09:50:29 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@786 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- StaticAnalysis/org.eclipse.epsilon.eol.visitor.resolution.type/src/org/eclipse/epsilon/eol/visitor/resolution/type/operationDefinitionUtil/StandardLibraryOperationDefinitionContainer.java
+++ StaticAnalysis/org.eclipse.epsilon.eol.visitor.resolution.type/src/org/eclipse/epsilon/eol/visitor/resolution/type/operationDefinitionUtil/StandardLibraryOperationDefinitionContainer.java
@@ -5,39 +5,20 @@ import java.util.ArrayList;
 import org.eclipse.epsilon.eol.metamodel.*;
 import org.eclipse.epsilon.eol.visitor.resolution.type.context.TypeResolutionContext;
 import org.eclipse.epsilon.eol.visitor.resolution.type.context.TypeUtil;
-import org.eclipse.epsilon.eol.ast2eol.util.Ast2DomUtil;
+import org.eclipse.epsilon.eol.ast2eol.util.Ast2EolUtil;
 
 public class StandardLibraryOperationDefinitionContainer extends OperationDefinitionContainer{
 
-	protected Ast2DomUtil ast2DomUtil = new Ast2DomUtil();
+	protected Ast2EolUtil ast2EolUtil = new Ast2EolUtil();
 	public StandardLibraryOperationDefinitionContainer(TypeUtil typeUtil) {
 		super(typeUtil);
 		init();
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void putOperationTypeAny()
+	public void registerOperation(String filename)
 	{
-		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeAny.eol");
-		for(OperationDefinition operationDefinition : program.getOperations())
-		{
-			Type contextType = operationDefinition.getContextType(); //get the contextType
-			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
-			
-			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
-			{
-				argTypes.add(v.getResolvedType()); //resolve and collect argument types
-			}
-			
-			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
-				putOperation(operationDefinition);
-			}
-		}
-	}
-	
-	public void putOperationTypeInteger()
-	{
-		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeInteger.eol");
+		EolProgram program = ast2EolUtil.generate(getClass(), filename);
 		for(OperationDefinition operationDefinition : program.getOperations())
 		{
 			Type contextType = operationDefinition.getContextType(); //get the contextType
@@ -55,100 +36,145 @@ public class StandardLibraryOperationDefinitionContainer extends OperationDefini
 
 	}
 	
-	public void putOperationTypeReal()
-	{
-		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeReal.eol");
-		for(OperationDefinition operationDefinition : program.getOperations())
-		{
-			Type contextType = operationDefinition.getContextType(); //get the contextType
-			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
-			
-			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
-			{
-				argTypes.add(v.getResolvedType()); //resolve and collect argument types
-			}
-			
-			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
-				putOperation(operationDefinition);
-			}
-		}
-		
-	}
-	
-	public void putOperationTypeString()
-	{
-		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeString.eol");
-		for(OperationDefinition operationDefinition : program.getOperations())
-		{
-			Type contextType = operationDefinition.getContextType(); //get the contextType
-			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
-			
-			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
-			{
-				argTypes.add(v.getResolvedType()); //resolve and collect argument types
-			}
-			
-			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
-				putOperation(operationDefinition);
-			}
-		}
-		
-	}
-	
-	public void putOperationTypeCollection()
-	{
-		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeCollection.eol");
-		for(OperationDefinition operationDefinition : program.getOperations())
-		{
-			Type contextType = operationDefinition.getContextType(); //get the contextType
-			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
-			
-			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
-			{
-				argTypes.add(v.getResolvedType()); //resolve and collect argument types
-			}
-			
-			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
-				putOperation(operationDefinition);
-			}
-		}
-		
-	}
-	
-	public void putOperationTypeModeElement()
-	{
-		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeModelElement.eol");
-		for(OperationDefinition operationDefinition : program.getOperations())
-		{
-			Type contextType = operationDefinition.getContextType(); //get the contextType
-			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
-			
-			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
-			{
-				argTypes.add(v.getResolvedType()); //resolve and collect argument types
-			}
-			
-			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
-				putOperation(operationDefinition);
-			}
-		}
-		
-	}
+//	public void putOperationTypeAny()
+//	{
+//		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeAny.eol");
+//		for(OperationDefinition operationDefinition : program.getOperations())
+//		{
+//			Type contextType = operationDefinition.getContextType(); //get the contextType
+//			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
+//			
+//			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
+//			{
+//				argTypes.add(v.getResolvedType()); //resolve and collect argument types
+//			}
+//			
+//			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
+//				putOperation(operationDefinition);
+//			}
+//		}
+//	}
+//	
+//	public void putOperationTypeInteger()
+//	{
+//		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeInteger.eol");
+//		for(OperationDefinition operationDefinition : program.getOperations())
+//		{
+//			Type contextType = operationDefinition.getContextType(); //get the contextType
+//			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
+//			
+//			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
+//			{
+//				argTypes.add(v.getResolvedType()); //resolve and collect argument types
+//			}
+//			
+//			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
+//				putOperation(operationDefinition);
+//			}
+//		}
+//
+//	}
+//	
+//	public void putOperationTypeReal()
+//	{
+//		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeReal.eol");
+//		for(OperationDefinition operationDefinition : program.getOperations())
+//		{
+//			Type contextType = operationDefinition.getContextType(); //get the contextType
+//			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
+//			
+//			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
+//			{
+//				argTypes.add(v.getResolvedType()); //resolve and collect argument types
+//			}
+//			
+//			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
+//				putOperation(operationDefinition);
+//			}
+//		}
+//		
+//	}
+//	
+//	public void putOperationTypeString()
+//	{
+//		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeString.eol");
+//		for(OperationDefinition operationDefinition : program.getOperations())
+//		{
+//			Type contextType = operationDefinition.getContextType(); //get the contextType
+//			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
+//			
+//			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
+//			{
+//				argTypes.add(v.getResolvedType()); //resolve and collect argument types
+//			}
+//			
+//			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
+//				putOperation(operationDefinition);
+//			}
+//		}
+//		
+//	}
+//	
+//	public void putOperationTypeCollection()
+//	{
+//		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeCollection.eol");
+//		for(OperationDefinition operationDefinition : program.getOperations())
+//		{
+//			Type contextType = operationDefinition.getContextType(); //get the contextType
+//			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
+//			
+//			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
+//			{
+//				argTypes.add(v.getResolvedType()); //resolve and collect argument types
+//			}
+//			
+//			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
+//				putOperation(operationDefinition);
+//			}
+//		}
+//		
+//	}
+//	
+//	public void putOperationTypeModeElement()
+//	{
+//		EolProgram program = ast2DomUtil.generate(getClass(), "operationTypeModelElement.eol");
+//		for(OperationDefinition operationDefinition : program.getOperations())
+//		{
+//			Type contextType = operationDefinition.getContextType(); //get the contextType
+//			ArrayList<Type> argTypes = new ArrayList<Type>(); //prepare argTypes
+//			
+//			for(VariableDeclarationExpression v: operationDefinition.getParameters()) //process each arg type
+//			{
+//				argTypes.add(v.getResolvedType()); //resolve and collect argument types
+//			}
+//			
+//			if (!containsOperation(operationDefinition.getName().getName(), contextType, argTypes)) {
+//				putOperation(operationDefinition);
+//			}
+//		}
+//		
+//	}
 	
 	public void init()
 	{
-		putOperationTypeAny();
-		putOperationTypeInteger();
-		putOperationTypeReal();
-		putOperationTypeString();
-		putOperationTypeCollection();
-		putOperationTypeModeElement();
+		registerOperation("operationTypeAny.eol");
+		registerOperation("operationTypeCollection.eol");
+		registerOperation("operationTypeInteger.eol");
+		registerOperation("operationTypeModelElement.eol");
+		registerOperation("operationTypeReal.eol");
+		registerOperation("operationTypeString.eol");
+//		putOperationTypeAny();
+//		putOperationTypeInteger();
+//		putOperationTypeReal();
+//		putOperationTypeString();
+//		putOperationTypeCollection();
+//		putOperationTypeModeElement();
 	}
 	
 	
-	public Ast2DomUtil getAst2DomUtil()
+	public Ast2EolUtil getAst2DomUtil()
 	{
-		return ast2DomUtil;
+		return ast2EolUtil;
 	}
 	
 	public static void main(String[] args) {
