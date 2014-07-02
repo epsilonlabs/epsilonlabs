f126b37b499bc8e5bff0162f24e02333e0d3234e
Ran Wei <ran.wei@york.ac.uk>
2014-07-02 13:31:44 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@864 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- StaticAnalysis/org.eclipse.epsilon.etl.visitor.resolution.type/src/org/eclipse/epsilon/etl/visitor/resolution/type/context/EtlTypeResolutionContext.java
+++ StaticAnalysis/org.eclipse.epsilon.etl.visitor.resolution.type/src/org/eclipse/epsilon/etl/visitor/resolution/type/context/EtlTypeResolutionContext.java
@@ -1,5 +1,10 @@
 package org.eclipse.epsilon.etl.visitor.resolution.type.context;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.ENamedElement;
 import org.eclipse.epsilon.eol.metamodel.AnnotationBlock;
 import org.eclipse.epsilon.eol.metamodel.SimpleAnnotation;
 import org.eclipse.epsilon.eol.visitor.resolution.type.context.TypeResolutionContext;
@@ -7,6 +12,8 @@ import org.eclipse.epsilon.etl.metamodel.TransformationRule;
 
 public class EtlTypeResolutionContext extends TypeResolutionContext{
 
+	//protected HashMap<TransformationRule, ArrayList<ENamedElement>> transformation
+	
 	public boolean isAbstract(TransformationRule rule)
 	{
 		AnnotationBlock annot = rule.getAnnotationBlock();
