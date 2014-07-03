407573179f42ab7ebc662bb3645d61f63e4516a0
Ran Wei <ran.wei@york.ac.uk>
2014-07-03 10:29:30 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@868 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- StaticAnalysis/org.eclipse.epsilon.eol.ast2eol/src/org/eclipse/epsilon/eol/ast2eol/SpecialAssignmentStatementCreator.java
+++ StaticAnalysis/org.eclipse.epsilon.eol.ast2eol/src/org/eclipse/epsilon/eol/ast2eol/SpecialAssignmentStatementCreator.java
@@ -1,20 +1,29 @@
 package org.eclipse.epsilon.eol.ast2eol;
 
 import org.eclipse.epsilon.common.parse.AST;
+import org.eclipse.epsilon.eol.metamodel.EolElement;
+import org.eclipse.epsilon.eol.metamodel.Expression;
+import org.eclipse.epsilon.eol.metamodel.SpecialAssignmentStatement;
 import org.eclipse.epsilon.eol.parse.EolParser;
 
 public class SpecialAssignmentStatementCreator extends AssignmentStatementCreator{
 	
 	@Override
 	public boolean appliesTo(AST ast) {
-		boolean condition = ast.getType() == EolParser.SPECIAL_ASSIGNMENT;
-		if(condition)
-		{
-			return true;
-		}
-		else {
-			return false;
-		}
+		return ast.getType() == EolParser.SPECIAL_ASSIGNMENT;
 	}
+	
+	@Override
+	public EolElement create(AST ast, EolElement container,
+			Ast2EolContext context) {
+		SpecialAssignmentStatement statement = context.getEolFactory().createSpecialAssignmentStatement(); //create AssignmentStatement
+		this.setAssets(ast, statement, container);
+		
+		statement.setLhs((Expression) context.getEolElementCreatorFactory().createDomElement(ast.getChild(0), statement, context)); //set Lhs
+		statement.setRhs((Expression) context.getEolElementCreatorFactory().createDomElement(ast.getChild(1), statement, context)); //set Rhs
+
+		return statement;
+	}
+
 
 }
