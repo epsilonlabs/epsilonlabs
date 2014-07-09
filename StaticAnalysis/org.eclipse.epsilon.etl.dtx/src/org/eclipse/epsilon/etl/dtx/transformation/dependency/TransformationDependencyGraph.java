d4cb7ecc56b39441719f08e1d888cbf253c83df7
Ran Wei <ran.wei@york.ac.uk>
2014-07-09 16:11:10 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@897 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- StaticAnalysis/org.eclipse.epsilon.etl.dtx/src/org/eclipse/epsilon/etl/dtx/transformation/dependency/TransformationDependencyGraph.java
+++ StaticAnalysis/org.eclipse.epsilon.etl.dtx/src/org/eclipse/epsilon/etl/dtx/transformation/dependency/TransformationDependencyGraph.java
@@ -1,22 +1,11 @@
 package org.eclipse.epsilon.etl.dtx.transformation.dependency;
 
-import java.util.HashMap;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.epsilon.common.dt.util.EclipseUtil;
-import org.eclipse.epsilon.eol.metamodel.EolElement;
-import org.eclipse.epsilon.eol.metamodel.TextRegion;
-import org.eclipse.epsilon.eol.parse.Eol_EolParserRules.newExpression_return;
 import org.eclipse.epsilon.etl.dtx.editor.EtlxEditor;
-import org.eclipse.epsilon.etl.metamodel.EtlProgram;
-import org.eclipse.epsilon.etl.metamodel.RuleDependency;
-import org.eclipse.epsilon.etl.metamodel.TransformationRule;
 import org.eclipse.epsilon.etl.visitor.coverage.analysis.impl.EtlCoverageAnalyser;
 import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.text.IDocument;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
@@ -24,27 +13,19 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.ViewPart;
-import org.eclipse.zest.core.widgets.Graph;
-import org.eclipse.zest.core.widgets.GraphConnection;
-import org.eclipse.zest.core.widgets.GraphItem;
-import org.eclipse.zest.core.widgets.GraphNode;
-import org.eclipse.zest.core.widgets.ZestStyles;
-import org.eclipse.zest.layouts.LayoutStyles;
-import org.eclipse.zest.layouts.algorithms.HorizontalTreeLayoutAlgorithm;
-import org.eclipse.zest.layouts.algorithms.SpringLayoutAlgorithm;
-import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
 
 public class TransformationDependencyGraph extends ViewPart {
 
 	  public static final String ID = "org.eclipse.epsilon.etl.dtx.transformation.dependency";
-	  private Graph graph;
-	  private int layout = 1;
 	  
-	  private Composite parent;
-	  protected HashMap<GraphItem, EolElement> graphMap = new HashMap<GraphItem, EolElement>(); 
+	  protected TransformationDependencyViewer transformationDependencyViewer;
+	  
 	  protected EtlCoverageAnalyser coverageAnalyser = new EtlCoverageAnalyser();
+	  
+	  private CTabFolder folder;
+	  private Composite parent;
+
 
 	  public TransformationDependencyGraph() {
 		  super();
@@ -65,51 +46,22 @@ public class TransformationDependencyGraph extends ViewPart {
 	  }
 	@Override
 	public void createPartControl(Composite parent) {
-		this.parent = parent;
-		EtlxEditor leEditor = getEditor();
-		if (leEditor.getEolLibraryModule() != null) {
-			graph = getDependencyGraph((EtlProgram) leEditor.getEolLibraryModule(), parent);
-			coverageAnalyser.run(leEditor.getEolLibraryModule());
-			System.out.println(coverageAnalyser.getContext().toString());
-		}
 		
-		
-//		graph = new Graph(parent, SWT.NONE);
-//		
-//		
-//		
-//	    // now a few nodes
-//	    GraphNode node1 = new GraphNode(graph, SWT.NONE, "Jim");
-//	    GraphNode node2 = new GraphNode(graph, SWT.NONE, "Jack");
-//	    GraphNode node3 = new GraphNode(graph, SWT.NONE, "Joe");
-//	    GraphNode node4 = new GraphNode(graph, SWT.NONE, "Bill");
-//	    // Lets have a directed connection
-//	    new GraphConnection(graph, ZestStyles.CONNECTIONS_DIRECTED, node1,
-//	        node2);
-//	    // Lets have a dotted graph connection
-//	    new GraphConnection(graph, ZestStyles.CONNECTIONS_DOT, node2, node3);
-//	    // Standard connection
-//	    new GraphConnection(graph, SWT.NONE, node3, node1);
-//	    // Change line color and line width
-//	    GraphConnection graphConnection = new GraphConnection(graph, SWT.NONE,
-//	        node1, node4);
-//	    graphConnection.changeLineColor(parent.getDisplay().getSystemColor(SWT.COLOR_GREEN));
-//	    // Also set a text
-//	    graphConnection.setText("This is a text");
-//	    graphConnection.setHighlightColor(parent.getDisplay().getSystemColor(SWT.COLOR_RED));
-//	    graphConnection.setLineWidth(3);
-//
-//	    graph.setLayoutAlgorithm(new SpringLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-//	    // Selection listener on graphConnect or GraphNode is not supported
-//	    // see https://bugs.eclipse.org/bugs/show_bug.cgi?id=236528
-//	    graph.addSelectionListener(new SelectionAdapter() {
-//	      @Override
-//	      public void widgetSelected(SelectionEvent e) {
-//	        System.out.println(e);
-//	      }
-//
-//	    });
+		folder = new CTabFolder(parent, SWT.NONE);
+		folder.setSimple(true);
+		folder.setTabPosition(SWT.BOTTOM);
 
+		CTabItem overviewItem = new CTabItem(folder, SWT.NONE);
+		overviewItem.setText("Overview");
+		transformationDependencyViewer = new TransformationDependencyViewer(folder, SWT.NONE);
+		overviewItem.setControl(transformationDependencyViewer);
+		
+		CTabItem coverageItem = new CTabItem(folder, SWT.NONE);
+		coverageItem.setText("Coverage Analysis");
+		
+		
+		folder.setSelection(overviewItem);
+		this.parent = parent;
 		contributeToActionBars();
 	}
 	
@@ -125,106 +77,19 @@ public class TransformationDependencyGraph extends ViewPart {
 		manager.add(new RefreshTransformationDependencyViewAction(this));
 	}
 
-	public void setLayoutManager() {
-	    switch (layout) {
-	    case 1:
-	      graph.setLayoutAlgorithm(new TreeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-	      layout++;
-	      break;
-	    case 2:
-	      graph.setLayoutAlgorithm(new SpringLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-	      layout = 1;
-	      break;
-
-	    }
-
-	  }
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
 
 	}
-	
-	public Graph getDependencyGraph(EtlProgram etlProgram, Composite parent)
-	{
-		Graph g = new Graph(parent, SWT.NONE);
-		HashMap<TransformationRule, GraphNode> map = new HashMap<TransformationRule, GraphNode>();
-		
-		for(TransformationRule rule: etlProgram.getTransformationRules())
-		{
-			GraphNode node = new GraphNode(g, SWT.NONE, rule.getName().getName());
-			graphMap.put(node, rule);
-			map.put(rule, node);
-			
-			for(RuleDependency dependency: rule.getResolvedRuleDependencies())
-			{
-				TransformationRule dependingRule = dependency.getDependingRule();
-				if (map.containsKey(dependingRule)) {
-					
-					GraphNode leNode = map.get(dependingRule);
-					
-					GraphConnection connection = new GraphConnection(g, ZestStyles.CONNECTIONS_DIRECTED, node,
-					        leNode);
-					graphMap.put(connection, dependency.getSourceElement());
-					connection.setText("depends on");
-					if (dependingRule.equals(rule)) {
-						connection.setCurveDepth(30);
-					}
-				}
-				else {
-					GraphNode leNode = new GraphNode(g, SWT.NONE, rule.getName().getName());
-					map.put(dependingRule, leNode);
-					GraphConnection connection = new GraphConnection(g, ZestStyles.CONNECTIONS_DIRECTED, node,
-					        leNode);
-					connection.setText("depends on");
-				}
-			}
-		}
-		g.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				
-				EolElement selectedElement = graphMap.get(e.item);
-				if (selectedElement != null) {
-					EtlxEditor editor = getEditor();
-					try {
-						IDocument doc = editor.getDocumentProvider().getDocument(editor.getEditorInput());
-						TextRegion region = selectedElement.getRegion();
-						
-						int startOffset = doc.getLineOffset(region.getStart().getLine()-1) + region.getStart().getColumn();
-						int endOffset = doc.getLineOffset(region.getEnd().getLine()-1) + region.getEnd().getColumn();
-						
-						FileEditorInput fileInputEditor = (FileEditorInput) editor.getEditorInput();
-						IFile file = fileInputEditor.getFile();
 
-						EclipseUtil.openEditorAt(file, region.getStart().getLine(), 
-								region.getStart().getColumn(), endOffset - startOffset, false);
-
-					} catch (Exception e2) {
-						e2.printStackTrace();
-					}
-					
-				}
-			}
-				
-		});
-		g.setLayoutAlgorithm(new HorizontalTreeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-		//g.setLayoutAlgorithm(new DirectedGraphLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-		//g.setLayoutAlgorithm(new SpringLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-		return g;
-	}
 	
 	public void refresh(){
-		if (graph != null) {
-			graph.dispose();	
-		}
-		EtlxEditor leEditor = getEditor();
-		if (leEditor.getEolLibraryModule() != null) {
-			graph = getDependencyGraph((EtlProgram) leEditor.getEolLibraryModule(), parent);
-		}
-		graph.applyLayout();
-		parent.layout();
+		transformationDependencyViewer.refresh();
+//		folder.layout();
+//		parent.layout();
+
 	}
 	
 	
