be6f3787ef6a02a69230c4ea65605b3023a770b1
Ran Wei <ran.wei@york.ac.uk>
2014-08-04 14:40:37 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@956 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

---
+++ StaticAnalysis/org.eclipse.epsilon.eol.analysis.suboptimal.performance.detection/src/org/eclipse/epsilon/eol/analysis/suboptimal/performance/detection/context/SuboptimalDetectionContext.java
@@ -0,0 +1,14 @@
+package org.eclipse.epsilon.eol.analysis.suboptimal.performance.detection.context;
+
+import log.LogBook;
+
+public class SuboptimalDetectionContext {
+
+	protected LogBook logBook = new LogBook();
+	
+	
+	public LogBook getLogBook() {
+		return logBook;
+	}
+	
+}
