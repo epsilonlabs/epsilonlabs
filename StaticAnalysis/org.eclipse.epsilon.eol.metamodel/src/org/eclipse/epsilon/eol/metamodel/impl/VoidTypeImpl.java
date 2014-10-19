4b39baedfe1dc99be807bcb2e06f4964103d9ea2
Ran Wei <ran.wei@york.ac.uk>
2014-10-19 13:23:40 +0000

    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@1029 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

---
+++ StaticAnalysis/org.eclipse.epsilon.eol.metamodel/src/org/eclipse/epsilon/eol/metamodel/impl/VoidTypeImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package org.eclipse.epsilon.eol.metamodel.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.epsilon.eol.metamodel.EolPackage;
+import org.eclipse.epsilon.eol.metamodel.VoidType;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Void Type</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class VoidTypeImpl extends TypeImpl implements VoidType {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected VoidTypeImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return EolPackage.Literals.VOID_TYPE;
+	}
+
+} //VoidTypeImpl
