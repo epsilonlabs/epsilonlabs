18aa492b070a78a43ca7e67784cde2112fcb48f5
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2014-12-07 23:58:46 +0000

    Added a minimal example
    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@1115 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

---
+++ StaticAnalysis/org.eclipse.epsilon.emc.emf.experiment/src/org/eclipse/epsilon/emc/emf/dimitris/EmfSmartModelWorkbench.java
@@ -0,0 +1,33 @@
+package org.eclipse.epsilon.emc.emf.dimitris;
+
+import java.util.ArrayList;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.epsilon.emc.emf.EmfSmartModel;
+import org.eclipse.epsilon.eol.analysis.optimisation.loading.context.ModelContainer;
+
+public class EmfSmartModelWorkbench {
+	
+	public static void main(String[] args) throws Exception {
+		
+		EmfSmartModel model = new EmfSmartModel();
+		
+		model.setName("M");
+		model.setReadOnLoad(true);
+		model.setModelFileUri(URI.createURI(EmfSmartModelWorkbench.class.getResource("filesystem.ecore").toString()));
+		
+		ModelContainer container = new ModelContainer("M");
+		container.addToModelElementsAllOfKind("EPackage");
+		ArrayList<ModelContainer> containers = new ArrayList<ModelContainer>();
+		containers.add(container);
+		
+		model.setModelContainers(containers);
+		model.load();
+		
+		System.out.println(model.allContents().size()); 
+		// I'd expect this to return 1 (only load the filesystem EPackage)
+		// but it seems to be loading everything instead
+		
+	}
+	
+}
