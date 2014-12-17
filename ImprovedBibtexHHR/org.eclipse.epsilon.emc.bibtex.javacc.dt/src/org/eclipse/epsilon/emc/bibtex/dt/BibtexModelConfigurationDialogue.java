ef7ca0c4d8aec3ae3bcc3ede5483d56f9bcce482
Horacio Hoyos-Rodriguez <hhr502@york.ac.uk>
2014-12-17 14:21:45 +0000

    - Added saving options to dialogue
    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@1138 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

--- ImprovedBibtexHHR/org.eclipse.epsilon.emc.bibtex.javacc.dt/src/org/eclipse/epsilon/emc/bibtex/dt/BibtexModelConfigurationDialogue.java
+++ ImprovedBibtexHHR/org.eclipse.epsilon.emc.bibtex.javacc.dt/src/org/eclipse/epsilon/emc/bibtex/dt/BibtexModelConfigurationDialogue.java
@@ -10,21 +10,38 @@
  ******************************************************************************/
 package org.eclipse.epsilon.emc.bibtex.dt;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.epsilon.common.dt.launching.dialogs.AbstractCachedModelConfigurationDialog;
 import org.eclipse.epsilon.emc.bibtex.BibtexModel;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 
 
 public class BibtexModelConfigurationDialogue extends AbstractCachedModelConfigurationDialog {
 
 	private static final String PROPERTY_MODEL_URI = BibtexModel.PROPERTY_MODEL_URI;
+	private static final String CAPITALIZED_TYPE = BibtexModel.CAPITALIZED_TYPE;
+	private static final String SORT_BY_KEY = BibtexModel.SORT_BY_KEY;
+	private static final String PREFERRED_ACCENT = BibtexModel.PREFERRED_ACCENT;
 
-	private Text modelFileText;
+	protected Text modelFileText;
+
+	protected Button capitalizedTypeNameBtn;
+
+	private Button sortByKeyBtn;
+
+	private Button preferredAccentsBtn;
+	
+	private Composite bibtexGroup;
 	
 	
 	@Override
@@ -37,6 +54,29 @@ public class BibtexModelConfigurationDialogue extends AbstractCachedModelConfigu
 		super.createGroups(control);
 		createFilesGroup(control);
 		createLoadStoreOptionsGroup(control);
+		createFormatingGroup(control);
+		bibtexGroup.setEnabled(false);
+		for (Control c : bibtexGroup.getChildren())
+	         c.setEnabled(false);
+		
+		// TODO Hook storeOnDisposalCheckbox to enable formatting options
+		storeOnDisposalCheckbox.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				boolean selection = storeOnDisposalCheckbox.getSelection();
+				bibtexGroup.setEnabled(selection);
+				for (Control c : bibtexGroup.getChildren())
+			         c.setEnabled(selection);
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 	}
 	
 	@Override
@@ -50,14 +90,24 @@ public class BibtexModelConfigurationDialogue extends AbstractCachedModelConfigu
 		if (properties == null) return;
 
 		// Restore values from legacy launch configuration
-		modelFileText.setText(properties.getProperty(PROPERTY_MODEL_URI));
+		String stringURI = properties.getProperty(PROPERTY_MODEL_URI);
+		URI platformURI = URI.createURI(stringURI);
+		String fileText = "";
+		if (platformURI.isPlatform())
+			fileText =  platformURI.toPlatformString(false);
+		modelFileText.setText(fileText);
+		capitalizedTypeNameBtn.setSelection(properties.getBooleanProperty(CAPITALIZED_TYPE, false));
+		sortByKeyBtn.setSelection(properties.getBooleanProperty(SORT_BY_KEY, false));
+		preferredAccentsBtn.setSelection(properties.getBooleanProperty(PREFERRED_ACCENT, false));
 	}
 	
 	@Override
 	protected void storeProperties(){
 		super.storeProperties();
-
-		properties.put(PROPERTY_MODEL_URI, modelFileText.getText());
+		properties.put(PROPERTY_MODEL_URI, URI.createPlatformResourceURI(modelFileText.getText(), false));
+		properties.put(CAPITALIZED_TYPE, capitalizedTypeNameBtn.getSelection());
+		properties.put(SORT_BY_KEY, sortByKeyBtn.getSelection());
+		properties.put(PREFERRED_ACCENT, preferredAccentsBtn.getSelection());
 	}
 
 	protected Composite createFilesGroup(Composite parent) {
@@ -80,4 +130,16 @@ public class BibtexModelConfigurationDialogue extends AbstractCachedModelConfigu
 		groupContent.pack();
 		return groupContent;
 	}	
+	
+	protected void createFormatingGroup(Composite parent) {
+		bibtexGroup = createGroupContainer(parent, "Bibtex Format", 4);
+		capitalizedTypeNameBtn = new Button(bibtexGroup, SWT.CHECK);
+		capitalizedTypeNameBtn.setText("Use Capitals for Publication type");
+		sortByKeyBtn = new Button(bibtexGroup, SWT.CHECK);
+		sortByKeyBtn.setText("Sort entries by key");
+		preferredAccentsBtn = new Button(bibtexGroup, SWT.CHECK);
+		preferredAccentsBtn.setText("Use preferred accent mode");
+		preferredAccentsBtn.setToolTipText("Use the preferred \\'{o} instead of \\'o for accents.");
+		
+	}
 }
