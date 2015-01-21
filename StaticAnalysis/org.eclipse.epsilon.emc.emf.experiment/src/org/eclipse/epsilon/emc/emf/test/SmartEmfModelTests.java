9f9b04a6e1c67e0bfb91e997ca5a59d0cc5bc23c
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2015-01-21 15:58:20 +0000

    Added a couple of unit tests
    git-svn-id: http://epsilonlabs.googlecode.com/svn/trunk@1169 25f02280-7a2e-11de-a1b3-85d0aeae5c5b

---
+++ StaticAnalysis/org.eclipse.epsilon.emc.emf.experiment/src/org/eclipse/epsilon/emc/emf/test/SmartEmfModelTests.java
@@ -0,0 +1,58 @@
+package org.eclipse.epsilon.emc.emf.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.epsilon.emc.emf.EmfModelResourceFactory;
+import org.eclipse.epsilon.emc.emf.EmfSmartModel;
+import org.eclipse.epsilon.emc.emf.SmartEmfModelResourceFactory;
+import org.eclipse.epsilon.eol.analysis.optimisation.loading.context.ModelContainer;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SmartEmfModelTests {
+	
+	@Before
+	public void teardown() {
+		SmartEmfModelResourceFactory.getInstance().clearCache();
+	}
+	
+	@Test
+	public void testLoadAllOfTypeEClassifier() throws Exception {
+		ModelContainer container = new ModelContainer("ecore");
+		container.addToModelElementsAllOfType("EClassifier");
+		assertEquals(0, load(container).allContents().size());
+	}
+	
+	@Test
+	public void testLoadAllOfKindEClassifier() throws Exception {
+		ModelContainer container = new ModelContainer("ecore");
+		container.addToModelElementsAllOfKind("EClassifier");
+		
+		assertEquals(2, load(container).allContents().size());		
+	}
+	
+	public EmfSmartModel load(ModelContainer container) throws EolModelLoadingException {
+		EmfSmartModel model = new EmfSmartModel();
+		
+		model.setName("M");
+		model.setReadOnLoad(true);
+		model.setMetamodelUri(EcorePackage.eNS_URI);
+		model.setModelFileUri(URI.createURI(SmartEmfModelTests.class.getResource("filesystem.ecore").toString()));
+		
+		ArrayList<ModelContainer> containers = new ArrayList<ModelContainer>();
+		containers.add(container);
+		
+		model.setModelContainers(containers);
+		model.setSmartLoading(true);
+		model.preProcess();
+		model.load();
+		
+		return model;
+	}
+	
+}
